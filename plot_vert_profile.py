b65385e3ccab53df5f676b085c42a8c1c28d75ce
lilyjohnston <lily.johnston@noaa.gov>
2024-06-26 20:46:28 +0000

    vert profile updates

--- plot_vert_profile.py
+++ plot_vert_profile.py
@@ -1,8 +1,10 @@
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-fdir = '/Users/lilyjohnston/socrates/plot_socrates_obs/radiosonde_data'
+fdir = '/scratch1/BMC/gmtb/Dustin.Swales/Lapenta/Lily/data/OBS/soundings/'
+output_dir = '/home/Lily.Johnston/plot_socrates_obs/figures'
 filenames = ['SOCRATES_HighRes_20180204_1115_Melbourne.txt',
              'SOCRATES_HighRes_20180204_1219_ISS3.txt',
              'SOCRATES_HighRes_20180204_1715_Hobart.txt',
@@ -43,61 +45,68 @@ def process_file(filename):
                             data[varid].append(float(entry))
                         except ValueError:
                             print(f"skipping non-numeric value in {varid}: {entry}")
-    return None, None
+    return data
 
+def plot_data(data, filename):
+    fig,axes =plt.subplots(3,1,figsize=(10,15))
+    
+    #Temperature and dewpoint
+    ax1 = axes[0]
+    ax1.plot(data["Temp"][:],data["Press"],label='Temperature (°C)', color='blue')
+    ax1.plot(data["Dewpt"][:],data["Press"],label='Dewpoint (°C)', color='g')
+    ax1.set_title("Temperature & Dewpoint Profile")
+    ax1.set_ylabel('Pressure (mb)')
+    ax1.set_xlabel('Temperature (°C)')
+    ax1.set_yscale('log')
+    ax1.set_ylim(1000,10)
+    ax1.invert_yaxis()
+    ax1.legend()
+    ax2 = ax1.twinx()
+    ax2.plot(data["Temp"][:],data["Alt"][:], label='Altitude (m)', visible=False)
+    ax2.set_ylabel('Altitude (m)')
+    ax1.legend()
 
+    # Wind speed and direction
+    ax1 = axes[1]
+    fig,ax1 =plt.subplots()
+    ax1.plot(data["spd"][:],data["Press"],label='Wind Speed (m/s)', color='blue')
+    ax1.set_title("Wind Profile")
+    ax1.set_ylabel('Pressure (mb)')
+    ax1.set_xlabel('Wind speed (m/s)')
+    ax1.set_yscale('log')
+    ax1.set_ylim(1000,100)
+    ax1.set_xlim(0,50)
+    ax1.invert_yaxis()
+    ax2=ax1.twiny()
+    ax2.plot(data["dir"][:],data["Press"], label='Wind Direction (°)', color='g')
+    ax2.set_xlabel('Wind Direction (°)')
+    ax2.set_xlim(0,360)
+    handles1,labels1=ax1.get_legend_handles_labels()
+    handles2,labels2=ax2.get_legend_handles_labels()
+    handles=handles1+handles2
+    labels=labels1+labels2
+    ax1.legend(handles,labels)
 
-fig,ax1 =plt.subplots() 
-ax1.plot(data["Temp"][:],data["Press"],label='Temperature (°C)', color='blue')
-ax1.plot(data["Dewpt"][:],data["Press"],label='Dewpoint (°C)', color='g')
-ax1.set_title("Temperature & Dewpoint Profile")
-ax1.set_ylabel('Pressure (mb)')
-ax1.set_xlabel('Temperature (°C)')
-ax1.set_yscale('log')
-ax1.set_ylim(1000,10)
-ax2 = ax1.twinx()
-ax2.plot(data["Temp"][:],data["Alt"][:], label='Altitude (m)', visible=False)
-ax2.set_ylabel('Altitude (m)')
-ax1.legend()
-plt.savefig('Temperature_profile.png')
-fig.tight_layout()
-plt.show()
+    # Horizontal and verticle wind
+    ax = axes[2]
+    ax.plot(data["Ucmp"][:],data["Press"],label='U Wind (m/s)', color='blue')
+    ax.plot(data["Vcmp"][:],data["Press"],label='V Wind (m/s)', color='g')
+    ax.axvline(x=0,color='r',linestyle='--')
+    ax.set_title("Wind Profile")
+    ax.set_ylabel('Pressure (mb)')
+    ax.set_ylim(1000,100)
+    ax.set_xlabel('Wind speed (m/s)')
+    ax.set_xlim(-40,40)
+    ax.set_yscale('log')
+    ax.legend()
 
-# Wind speed and direction
-fig,ax1 =plt.subplots()
-ax1.plot(data["spd"][:],data["Press"],label='Wind Speed (m/s)', color='blue')
-ax1.set_title("Wind Profile")
-ax1.set_ylabel('Pressure (mb)')
-ax1.set_xlabel('Wind speed (m/s)')
-ax1.set_yscale('log')
-ax1.set_ylim(1000,100)
-ax1.set_xlim(0,50)
-ax2=ax1.twiny()
-ax2.plot(data["dir"][:],data["Press"], label='Wind Direction (°)', color='g')
-ax2.set_xlabel('Wind Direction (°)')
-ax2.set_xlim(0,360)
-handles1,labels1=ax1.get_legend_handles_labels()
-handles2,labels2=ax2.get_legend_handles_labels()
-handles=handles1+handles2
-labels=labels1+labels2
-plt.legend(handles,labels)
-plt.savefig('Wind_dir_spd_profile.png')
-fig.tight_layout()
-plt.show()
+    plt.tight_layout()
+    output_path = os.path.join(output_dir, f'{os.path.splitext(filename)[0]}.png')
+    plt.savefig(output_path)
+    plt.show()
+    plt.close(fig)
 
-# Horizontal and verticle wind
-fig,ax =plt.subplots()
-ax.plot(data["Ucmp"][:],data["Press"],label='U Wind (m/s)', color='blue')
-ax.plot(data["Vcmp"][:],data["Press"],label='V Wind (m/s)', color='g')
-ax.axvline(x=0,color='r',linestyle='--')
-ax.set_title("Wind Profile")
-ax.set_ylabel('Pressure (mb)')
-ax.set_ylim(1000,100)
-ax.set_xlabel('Wind speed (m/s)')
-ax.set_xlim(-40,40)
-ax.set_yscale('log')
-ax.legend()
-plt.savefig('u&v_profile.png')
-fig.tight_layout()
-plt.show()
+for filename in filenames:
+    data = process_file(filename)
+    plot_data(data, filename)
 
