32b19109babd17490d68c4d8fd327d14cda8586e
lilyjohnston <lily.johnston@noaa.gov>
2024-06-27 17:01:55 +0000

    trying to figure out land features

--- plot_ufs2d.py
+++ plot_ufs2d.py
@@ -7,16 +7,13 @@ from matplotlib import pyplot as plt
 import matplotlib.cm as cm
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-from cartopy.feature import ShapelyFeature, NaturalEarthFeature
-from netCDF4 import Dataset
 
 # Colormaps to use?
-cmap_div = plt.cm.bwr
 cmap_seq = plt.cm.YlGnBu
 
 # Number of levels for plots?
 nlev = 10
-nlevd = 25
+#nlevd = 25
 
 # Dictionary of variable(s) to plot
 var_dict = [\
@@ -51,7 +48,6 @@ map_south = -70
 map_north = -30
 
 # Some map projections                                                                                                                                          
-#map_proj = ccrs.LambertConformal(central_longitude=150.0, central_latitude=-40.0, standard_parallels=(-33, -45), cutoff=0)                                    
 map_proj = ccrs.PlateCarree(central_longitude=150)
 
 # For every file...
@@ -60,16 +56,13 @@ for ifile in range(0,len(fcast_hr)):
     file1  = dirRT + "/phyf" + fcast_hr[ifile] + ".nc"
 
     # Open datasets
-    data1 = xr.open_dataset(file1,concat_characters=True,decode_cf=True)
+    data1 = xr.open_dataset(file1, concat_characters=True, decode_cf=True)
 
     # For each variable...
-    for ivar in range(0, len(var_dict)):
+    for ivar in range(len(var_dict)):
         # Create output file.
         fileOUT = "ufs."+var_dict[ivar]["name"]+"."+fcast_hr[ifile] +".png"
-        
-        # Determine plot levels.
-        plot_levels = np.linspace(var_dict[ivar]["range"][0],  var_dict[ivar]["range"][1],  nlev)
-        
+        plot_levels = np.linspace(var_dict[ivar]["range"][0],  var_dict[ivar]["range"][1], nlev)                        
         # Create temporary plot variable for ease.
         var_name = var_dict[ivar]["name"]
         var_data = data1[var_name]
@@ -82,33 +75,25 @@ for ifile in range(0,len(fcast_hr)):
         fig = plt.figure(figsize=(12,8))
         ax = fig.add_subplot(1, 1, 1, projection= map_proj)
         ax.set_extent([map_west, map_east, map_south, map_north])
+        land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m', edgecolor='k', facecolor= 'none')
+        ocean_10m = cfeature.NaturalEarthFeature('physical', 'ocean', '10m', edgecolor='k', facecolor= 'none')
+        lakes_10m = cfeature.NaturalEarthFeature('physical', 'lakes', '10m', edgecolor='k', facecolor= 'none')
+        c_10m = cfeature.NaturalEarthFeature('cultural', 'admin_0_countries', '10m', edgecolor='k', facecolor= 'none')
 
-        #land
-        land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m', edgecolor='k')
-
-        # ocean
-        ocean_10m = cfeature.NaturalEarthFeature('physical', 'ocean', '10m, edgecolor='k')
-
-        # ocean                                                                                                                                                         
-        lakes_10m = cfeature.NaturalEarthFeature('physical', 'lakes', '10m', edgecolor='k')
-
-        # Countries                                                                                                                                                     
-        c_10m = cfeature.NaturalEarthFeature('cultural', 'admin_0_countries', '10m', edgecolor='k')
-
-        ax.add_feature(land_10m,zorder=1)
-        ax.add_feature(ocean_10m,zorder=2)
-        ax.add_feature(lakes_10m,zorder=3)
-        ax.add_feature(c_10m,zorder=4)
+        ax.add_feature(land_10m,zorder=2)
+        ax.add_feature(ocean_10m,zorder=3)
+        ax.add_feature(lakes_10m,zorder=4)
+        ax.add_feature(c_10m,zorder=5)
 
         # Make contour contourf
-        contourf_plot = ax.contourf(lon, lat, varPlt, levels=plot_levels, cmap=cmap_seq)
+        contourf_plot = ax.contourf(lon, lat, varPlt, levels=plot_levels, cmap=cmap_seq, zorder=1)
         cbar = plt.colorbar(contourf_plot, ax=ax, orientation='vertical')
-        cbar.set_label(f"{long_name}" units)
+        cbar.set_label(f"{long_name} ({units})")
         ax.set_title(f"{long_name} at {fcast_hr[ifile]} hours")
         ax.set_xlabel('Longitude')
         ax.set_ylabel('Latitude')
         # Save
         print(fileOUT)
-        fig.savefig(fileOUT)
+        fig.savefig(fileOUT, dpi=300, bbox_inches='tight')
         plt.show()
         plt.close(fig)
