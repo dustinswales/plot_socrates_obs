d5c2818fa8a9145816678b65fbc105ace54442f8
lilyjohnston <lily.johnston@noaa.gov>
2024-06-27 15:32:45 +0000

    adding land features

--- plot_ufs2d.py
+++ plot_ufs2d.py
@@ -4,6 +4,11 @@ import numpy as np
 import xarray as xr
 import os
 from matplotlib import pyplot as plt
+import matplotlib.cm as cm
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.feature import ShapelyFeature, NaturalEarthFeature
+from netCDF4 import Dataset
 
 # Colormaps to use?
 cmap_div = plt.cm.bwr
@@ -39,9 +44,16 @@ dirRT = "/scratch2/BMC/wrfruc/jensen/socrates/model_run/socrates_rf09_13km_old_2
 # Which forecast hour(s)?
 fcast_hr = ["000","024"]
 
-##########################################################################################
-# NO CHANGES BELOW
-##########################################################################################
+# Map dimensions                                                                                                                                                
+map_west  = 60
+map_east  = 180
+map_south = -70
+map_north = -30
+
+# Some map projections                                                                                                                                          
+#map_proj = ccrs.LambertConformal(central_longitude=150.0, central_latitude=-40.0, standard_parallels=(-33, -45), cutoff=0)                                    
+map_proj = ccrs.PlateCarree(central_longitude=150)
+
 # For every file...
 for ifile in range(0,len(fcast_hr)):
     # Get input file.
@@ -54,22 +66,45 @@ for ifile in range(0,len(fcast_hr)):
     for ivar in range(0, len(var_dict)):
         # Create output file.
         fileOUT = "ufs."+var_dict[ivar]["name"]+"."+fcast_hr[ifile] +".png"
-
+        
         # Determine plot levels.
         plot_levels = np.linspace(var_dict[ivar]["range"][0],  var_dict[ivar]["range"][1],  nlev)
-
+        
         # Create temporary plot variable for ease.
-        varPlt = data1[var_dict[ivar]["name"]][0,:,:].values
+        var_name = var_dict[ivar]["name"]
+        var_data = data1[var_name]
+        varPlt = var_data[0,:,:].values
         lon    = data1.grid_xt.values
         lat    = data1.grid_yt.values
-        
+        long_name = var_data.attrs.get("long_name", var_name)
+        units = var_data.attrs.get("units", "")
         # Set up figure
         fig = plt.figure(figsize=(12,8))
-        ax = fig.add_subplot(1, 1, 1)
+        ax = fig.add_subplot(1, 1, 1, projection= map_proj)
+        ax.set_extent([map_west, map_east, map_south, map_north])
+
+        #land
+        land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m', edgecolor='k')
+
+        # ocean
+        ocean_10m = cfeature.NaturalEarthFeature('physical', 'ocean', '10m, edgecolor='k')
+
+        # ocean                                                                                                                                                         
+        lakes_10m = cfeature.NaturalEarthFeature('physical', 'lakes', '10m', edgecolor='k')
+
+        # Countries                                                                                                                                                     
+        c_10m = cfeature.NaturalEarthFeature('cultural', 'admin_0_countries', '10m', edgecolor='k')
+
+        ax.add_feature(land_10m,zorder=1)
+        ax.add_feature(ocean_10m,zorder=2)
+        ax.add_feature(lakes_10m,zorder=3)
+        ax.add_feature(c_10m,zorder=4)
 
         # Make contour contourf
         contourf_plot = ax.contourf(lon, lat, varPlt, levels=plot_levels, cmap=cmap_seq)
-        ax.set_title(var_dict[ivar]["name"] + "at" + fcast_hr[ifile] + "hours")
+        cbar = plt.colorbar(contourf_plot, ax=ax, orientation='vertical')
+        cbar.set_label(f"{long_name}" units)
+        ax.set_title(f"{long_name} at {fcast_hr[ifile]} hours")
         ax.set_xlabel('Longitude')
         ax.set_ylabel('Latitude')
         # Save
