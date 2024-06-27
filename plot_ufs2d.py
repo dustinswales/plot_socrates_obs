db7207ea5eedf7092a92c86e7ff16b8267d830b9
lilyjohnston <lily.johnston@noaa.gov>
2024-06-27 19:48:58 +0000

    cannot figure out adding coastlines

--- plot_ufs2d.py
+++ plot_ufs2d.py
@@ -13,7 +13,7 @@ cmap_seq = plt.cm.YlGnBu
 
 # Number of levels for plots?
 nlev = 10
-#nlevd = 25
+nlevd = 25
 
 # Dictionary of variable(s) to plot
 var_dict = [\
@@ -47,46 +47,33 @@ map_east  = 180
 map_south = -70
 map_north = -30
 
-# Some map projections                                                                                                                                          
-map_proj = ccrs.PlateCarree(central_longitude=150)
-
+central_lon = 150
 # For every file...
 for ifile in range(0,len(fcast_hr)):
     # Get input file.
     file1  = dirRT + "/phyf" + fcast_hr[ifile] + ".nc"
-
     # Open datasets
     data1 = xr.open_dataset(file1, concat_characters=True, decode_cf=True)
 
     # For each variable...
-    for ivar in range(len(var_dict)):
+    for ivar in range(0,len(var_dict)):
         # Create output file.
         fileOUT = "ufs."+var_dict[ivar]["name"]+"."+fcast_hr[ifile] +".png"
         plot_levels = np.linspace(var_dict[ivar]["range"][0],  var_dict[ivar]["range"][1], nlev)                        
         # Create temporary plot variable for ease.
         var_name = var_dict[ivar]["name"]
         var_data = data1[var_name]
-        varPlt = var_data[0,:,:].values
-        lon    = data1.grid_xt.values
-        lat    = data1.grid_yt.values
-        long_name = var_data.attrs.get("long_name", var_name)
-        units = var_data.attrs.get("units", "")
+        varPlt = var_data[0, :, :].values
+        lon = data1.grid_xt.values
+        lat = data1.grid_yt.values
+        long_name = var_data.attrs.get('long_name', var_name)
+        units = var_data.attrs.get('units', '')
         # Set up figure
-        fig = plt.figure(figsize=(12,8))
-        ax = fig.add_subplot(1, 1, 1, projection= map_proj)
-        ax.set_extent([map_west, map_east, map_south, map_north])
-        land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m', edgecolor='k', facecolor= 'none')
-        ocean_10m = cfeature.NaturalEarthFeature('physical', 'ocean', '10m', edgecolor='k', facecolor= 'none')
-        lakes_10m = cfeature.NaturalEarthFeature('physical', 'lakes', '10m', edgecolor='k', facecolor= 'none')
-        c_10m = cfeature.NaturalEarthFeature('cultural', 'admin_0_countries', '10m', edgecolor='k', facecolor= 'none')
-
-        ax.add_feature(land_10m,zorder=2)
-        ax.add_feature(ocean_10m,zorder=3)
-        ax.add_feature(lakes_10m,zorder=4)
-        ax.add_feature(c_10m,zorder=5)
-
-        # Make contour contourf
-        contourf_plot = ax.contourf(lon, lat, varPlt, levels=plot_levels, cmap=cmap_seq, zorder=1)
+        fig = plt.figure(figsize=(12,18))
+        ax = fig.add_subplot(1,1,1, projection=ccrs.PlateCarree(central_longitude=central_lon))
+        ax.set_extent([map_west, map_east, map_south, map_north],crs=ccrs.PlateCarree())
+        ax.coastlines()
+        contourf_plot = ax.contourf(lon, lat, varPlt,levels=plot_levels, cmap=cmap_seq, transform=ccrs.PlateCarree())
         cbar = plt.colorbar(contourf_plot, ax=ax, orientation='vertical')
         cbar.set_label(f"{long_name} ({units})")
         ax.set_title(f"{long_name} at {fcast_hr[ifile]} hours")
