9afac08f65be56d6c07dd777037ba0f2d5e66364
lilyjohnston <lily.johnston@noaa.gov>
2024-06-28 21:57:43 +0000

    adding contour lines fixed

--- plot_ufs2d.py
+++ plot_ufs2d.py
@@ -7,17 +7,18 @@ from matplotlib import pyplot as plt
 import matplotlib.cm as cm
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import cmocean
 
-# Colormaps to use?
-cmap_seq = plt.cm.YlGnBu
+# Colormaps
+cmap_seq = cmocean.cm.deep
 
 # Number of levels for plots?
-nlev = 10
-nlevd = 25
+nlev = 100
+#nlevd = 25
 
 # Dictionary of variable(s) to plot
 var_dict = [\
-            {"name":"tmpsfc",       "range":[250, 340], "drange":[-.2,.2]}]#, \
+            #{"name":"tmpsfc",       "range":[250, 340], "drange":[-.2,.2]}, \
             #{"name":"tmp2m",        "range":[250, 340], "drange":[-.2,.2]}, \
             #{"name":"ulwrf_ave",    "range":[200, 600], "drange":[-.5,.5]}, \
             #{"name":"ulwrf_avetoa", "range":[  1, 400], "drange":[-.1,.1]}, \
@@ -32,7 +33,8 @@ var_dict = [\
             #{"name":"csulf",        "range":[200, 600], "drange":[-1,1]}, \
             #{"name":"csulftoa",     "range":[  1, 400], "drange":[-1,1]}, \
             #{"name":"csusf",        "range":[  1, 500], "drange":[-1,1]}, \
-            #{"name":"csusftoa",     "range":[  1,1300], "drange":[-1,1]}]
+            {"name":"csusftoa",     "range":[  1,1300], "drange":[-1,1]}
+            ]
 
 
 # RT root directory
@@ -43,8 +45,8 @@ fcast_hr = ["000","024"]
 
 # Map dimensions                                                                                                                                                
 map_west  = 60
-map_east  = 180
-map_south = -70
+map_east  = 200
+map_south = -75
 map_north = -30
 
 central_lon = 150
@@ -59,28 +61,32 @@ for ifile in range(0,len(fcast_hr)):
     for ivar in range(0,len(var_dict)):
         # Create output file.
         fileOUT = "ufs."+var_dict[ivar]["name"]+"."+fcast_hr[ifile] +".png"
-        plot_levels = np.linspace(var_dict[ivar]["range"][0],  var_dict[ivar]["range"][1], nlev)                        
+        plot_levels = np.linspace(0,500, nlev)                        
         # Create temporary plot variable for ease.
         var_name = var_dict[ivar]["name"]
         var_data = data1[var_name]
         varPlt = var_data[0, :, :].values
-        lon = data1.grid_xt.values
-        lat = data1.grid_yt.values
+        lon = data1.lon.values
+        lat = data1.lat.values
         long_name = var_data.attrs.get('long_name', var_name)
         units = var_data.attrs.get('units', '')
         # Set up figure
-        fig = plt.figure(figsize=(12,18))
+        fig = plt.figure(figsize=(14,10))
         ax = fig.add_subplot(1,1,1, projection=ccrs.PlateCarree(central_longitude=central_lon))
         ax.set_extent([map_west, map_east, map_south, map_north],crs=ccrs.PlateCarree())
         ax.coastlines()
         contourf_plot = ax.contourf(lon, lat, varPlt,levels=plot_levels, cmap=cmap_seq, transform=ccrs.PlateCarree())
-        cbar = plt.colorbar(contourf_plot, ax=ax, orientation='vertical')
+        cbar = plt.colorbar(contourf_plot, ax=ax, orientation='horizontal', ticks=np.arange(0,501,50))
+        contourf_plot.set_clim(0, 500)
         cbar.set_label(f"{long_name} ({units})")
         ax.set_title(f"{long_name} at {fcast_hr[ifile]} hours")
         ax.set_xlabel('Longitude')
         ax.set_ylabel('Latitude')
+        plt.subplots_adjust(left=0.05, right=0.9, top=0.15, bottom=0.1)
+        plt.tight_layout()
         # Save
         print(fileOUT)
         fig.savefig(fileOUT, dpi=300, bbox_inches='tight')
         plt.show()
         plt.close(fig)
+        
